cfe6724639ad9b8a1fe8879f5803742815d9b95a
Michael Chervonyak <74454331+MichaelChervonyak@users.noreply.github.com>
2021-01-01 04:49:36 +0100

    v0.0.2a3
    Timeline tools bar

--- Editor/Timeline.cs
+++ Editor/Timeline.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEditor;
-using System.Collections;
 
 class Timeline : EditorWindow
 {
     string myString = "Hello World";
+    string myString2 = "Hello World";
     bool groupEnabled;
     bool myBool = true;
     float myFloat = 1.23f;
 
+    private int animationLenth = 30;
+
+
+    private const int buttonWidth= 35;
+
     [MenuItem("Window/MichMond/Cinema Timeline")]
     public static void ShowWindow()
     {
-     var w=   EditorWindow.GetWindow(typeof(Timeline));
+        var w= EditorWindow.GetWindow(typeof(Timeline));
         w.minSize = new Vector2(450f, 250f);
     }
 
@@ -21,7 +26,13 @@ class Timeline : EditorWindow
     void OnGUI()
     {
         EditorGUILayout.BeginHorizontal();
+           
         /*   */ EditorGUILayout.BeginVertical(GUILayout.Width(125));
+        /*   *   */ GUILayout.BeginHorizontal();
+        /*   *   *   */ if (GUILayout.Button("+", GUILayout.Width(buttonWidth))) { animationLenth--; }
+        /*   *   *   */ myString2 = EditorGUILayout.TextField(myString2);
+        /*   *   *   */ if (GUILayout.Button("+", GUILayout.Width(buttonWidth))) { animationLenth++; }
+        /*   *   */ GUILayout.EndHorizontal();
         /*   *   */ GUILayout.Label("Base Settings", EditorStyles.boldLabel);
         /*   *   */ myString = EditorGUILayout.TextField("Text Field", myString);
         /*   *   */ groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
@@ -31,6 +42,12 @@ class Timeline : EditorWindow
         /*   */ EditorGUILayout.EndVertical();
         /*   */ scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
         /*   *   */ EditorGUILayout.BeginVertical();
+        /*   *   *   */ GUILayout.BeginHorizontal();
+        /*   *   *   *   */ if (GUILayout.Button("<1",GUILayout.Width(buttonWidth))) {  Debug.Log("Clicked Button");  }
+        /*   *   *   *   */ if (GUILayout.Button("1>", GUILayout.Width(buttonWidth))) {  Debug.Log("Clicked Button");  }
+        /*   *   *   *   */ if (GUILayout.Button("<24", GUILayout.Width(buttonWidth))) {  Debug.Log("Clicked Button");  }
+        /*   *   *   *   */ if (GUILayout.Button("24>", GUILayout.Width(buttonWidth))) {  Debug.Log("Clicked Button"); }
+        /*   *   *   */ GUILayout.EndHorizontal();
         /*   *   *   */ EditorGUILayout.BeginHorizontal();
         /*   *   *   *   */ GUILayout.Label("Base Settings", EditorStyles.boldLabel); myString = EditorGUILayout.TextField("Text Field", myString); groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled); EditorGUILayout.EndToggleGroup();
         /*   *   *   */ EditorGUILayout.EndHorizontal();
